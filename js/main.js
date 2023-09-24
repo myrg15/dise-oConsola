78326224b18f1070fd6645695cd6cc4317a181be
myrg15 <merishield@gmail.com>
2023-09-24 02:04:35 +0200

    inclusión de imagenes, reorganización botones, ingreso de javascript

--- js/main.js
+++ js/main.js
@@ -1,23 +1,53 @@
 
 let pantalla = document.getElementsByClassName ("screen2");
+let buttonA = document.getElementsByClassName ("screenimaganimeAmigosMovimiento");
+
+let buttonB = document.getElementsByClassName ("screenimaganimeVariosJuntos");
+let buttonSelect= document.getElementsByClassName("screenimaganimeUnaSola");
+let buttonStart= document.getElementsByClassName("screenimagpokemonAnimado");
+
+
 
 document.addEventListener("click", (e)=>{
-
-
     if(e.target.getElementsByClassName === 'power'){
 
         // pantalla.style.backgroundColor = "red";
         pantalla.getElementsByClassName("screen2");
 
-    } else if (e.target.id === 'button2') {
+   // } else if (e.target.id === 'button2') {
 
         // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('imag/animeVariosJuntos.jpg')";
+     //   pantalla.style.backgroundImage = "url('imag/animeVariosJuntos.jpg')";
 
-    } else if (e.target.id === 'button3'){
+    //} else if (e.target.id === 'button3'){
 
-        pantalla.style.backgroundImage = "url('imag/animeUnaSola.jpg')";
+      //  pantalla.style.backgroundImage = "url('imag/animeUnaSola.jpg')";
     }
 
  });
- 
+
+ document.addEventListener("click", (e)=>{
+    if (e.target.getElementsByClassName === 'button2'){
+        buttonA.getElementsByClassName("screenimaganimeAmigosMovimiento");
+    }
+ });
+
+document.addEventListener("click", (e)=> {
+    if (e.target.getElementsByClassName === 'button1'){
+        buttonB.getElementsByClassName("screenimaganimeVariosJuntos");
+    }
+});
+
+document.addEventListener("click", (e)=>{
+    if (e.target.getElementsByClassName === 'buttonSelect'){
+        buttonSelect.getElementsByClassName("screenimaganimeUnaSola");
+        
+    }
+
+});
+
+document.addEventListener("click", (e)=>{
+    if (e.target.getElementsByClassName === 'buttonStart'){
+        buttonStart.getElementsByClassName("screenimagpokemonAnimado");
+    }
+});
