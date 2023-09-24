da747cced5cf06440e3fe8a8b2f4baf6c4d50a62
myrg15 <merishield@gmail.com>
2023-09-25 00:08:43 +0200

    Probando botones con función querySelector JS

--- js/main.js
+++ js/main.js
@@ -1,60 +1,51 @@
-
-/*let pantalla = document.getElementById ("screen2");
-let buttonA = document.getElementsByClassName ("screenimaganimeAmigosMovimiento");
-
-let buttonB = document.getElementsByClassName ("screenimaganimeVariosJuntos");
-let buttonSelect= document.getElementsByClassName("screenimaganimeUnaSola");
-let buttonStart= document.getElementsByClassName("screenimagpokemonAnimado");
-
-
-
-document.addEventListener("click", (e)=>{
-    if(e.target.getElementById === 'power'){
-
+//Probando asignar eventos a los botones
+/*document.addEventListener('click', (e)=>{
+    let pantalla = document.getElementById ('.screen2');
+    if(e.target.getElementById === '.power'){
+        pantalla.classList.add('.power');
+    }
+});
         // pantalla.style.backgroundColor = "red";
-        
-
    // } else if (e.target.id === 'button2') {
-
         // pantalla.style.backgroundColor = "grey";
      //   pantalla.style.backgroundImage = "url('imag/animeVariosJuntos.jpg')";
-
     //} else if (e.target.id === 'button3'){
-
       //  pantalla.style.backgroundImage = "url('imag/animeUnaSola.jpg')";
-    } else pantalla.getElementById("screen2") 
-
- });
-
- document.addEventListener("click", (e)=>{
-    if (e.target.getElementsByClassName === 'button2'){
-        buttonA.getElementsByClassName("screenimaganimeAmigosMovimiento");
+     
+ document.addEventListener('click', (e)=>{
+    let buttonA = document.getElementById ('screenimaganimeAmigosMovimiento');
+    if (e.target.getElementById === '.button2'){
+        buttonA.classList.add('.button2');
     }
  });
 
-document.addEventListener("click", (e)=> {
-    if (e.target.getElementsByClassName === 'button1'){
-        buttonB.getElementsByClassName("screenimaganimeVariosJuntos");
+document.addEventListener('click', (e)=> {
+    let buttonB = document.getElementById ('screenimaganimeVariosJuntos');
+    if (e.target.getElementById === '.button1'){
+        buttonB.classList.add('.button1');
     }
 });
 
-document.addEventListener("click", (e)=>{
-    if (e.target.getElementsByClassName === 'buttonSelect'){
-        buttonSelect.getElementsByClassName("screenimaganimeUnaSola");
-        
+document.addEventListener('click', (e)=>{
+    let buttonSelect= document.getElementById('screenimaganimeUnaSola');
+    if (e.target.getElementById === '.buttonSelect'){
+        buttonSelect.classList.add('.buttonSelect');
     }
-
 });
 
-document.addEventListener("click", (e)=>{
-    if (e.target.getElementsByClassName === 'buttonStart'){
-        buttonStart.getElementsByClassName("screenimagpokemonAnimado");
+document.addEventListener('click', (e)=>{
+    let buttonStart= document.getElementById('screenimagpokemonAnimad');
+    if (e.target.getElementById === 'buttonStart'){
+        buttonStart.classList.add('buttonStart');
     }
 });*/
 
-function selectA(){
-    let screen = document.getElementById("screenimaganimeAmigosMovimiento");
-    screen.classList.add(".buttonSelect");
+//Probando con funciones
+/*function powerOn(){
+    let screen = document.querySelector('.screen2');
+   // let on = document.getElementById("idpowerlight");
+    screen.classList.toggle('on');
+    //on.classList.add("powershadow")
 }
 
 function clickbuttonA(){
@@ -67,22 +58,44 @@ function clickbuttonB(){
     screen.classList.add(".button1");
 }
 
-/*function powerOn(){
-    let screen = document.querySelector('.screen2');
-   // let on = document.getElementById("idpowerlight");
-    screen.classList.toggle('on');
-    //on.classList.add("powershadow")
+function selectA(){
+    let screen = document.getElementById("screenimaganimeAmigosMovimiento");
+    screen.classList.add(".buttonSelect");
 }
 
+function startA(){
+    let screen = document.getElementById("screenimaganimeAmigosMovimiento");
+    screen.classList.add(".buttonSelect");
+}*/
+
+//Probando con querySelector(selecciona el botón)
+//classList.toggle (botón cambien de pantalla al hacer clic)
+
+
+function powerOn(){
+    let screen = document.querySelector('.on');
+    screen.classList.toggle('.screen2');
+   }
+
 function clickbuttonB() {
-    var buttonB = document.querySelector('.button1');
+    let buttonB = document.querySelector('.button1');
     buttonB.classList.toggle('screenimaganimeAmigosMovimiento');
   }
   
-  function clickbuttonA() {
-    var buttonA = document.querySelector('.button2');
+function clickbuttonA() {
+    let buttonA = document.querySelector('.button2');
     buttonA.classList.toggle('screenimaganimeVariosJuntos');
-  }*/
+  }
+
+function selectA() {
+    let buttonSele = document.querySelector('.buttonSelect');
+    buttonSele.classList.toggle('.screenimaganimeUnaSola');
+  }
+  
+function startA() {
+    let buttonSta = document.querySelector('.buttonStart');
+    buttonSta.classList.toggle('.screenimagpokemonAnimado');
+  }
 
 
 
